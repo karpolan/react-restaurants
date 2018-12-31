1e7a72ceafadd1694672c303055a99c076aaadce
Anton Karpenko <karpolan@karpolan.com>
2018-12-31 12:31:49 +0200

    Sorting for Favorites added

--- src/containers/ListOfRestaurants.js
+++ src/containers/ListOfRestaurants.js
@@ -1,47 +1,21 @@
 import React, { Component } from "react";
 import { List, Filter } from "./../components";
-import { getStorage } from "../storage/storage";
-import { compareSortRestaurants } from "./../storage";
+import { getFilterdRestaurantsList } from "./../storage";
 
 export class ListOfRestaurants extends Component {
-  list = null;
-
   constructor(props) {
     super(props);
-    this.state = { 
-      searchText: '',
-      sortKind: 0 
+    this.state = {
+      searchText: "",
+      sortKind: 0
     };
   }
 
-  componentWillMount() {
-    this.list = [...getStorage().restaurants];
-  }
-
   getFilterdList() {
-    let result = [];
-
-    // Text Search filter (ignore case)
-    const search = this.state.searchText.trim().toLowerCase();
-    if (search !== '') {
-      for (let i = 0; i < this.list.length; i++) {
-        if (this.list[i].name.toLowerCase().includes(search)) result.push(this.list[i]);
-      }
-    } else result = [...this.list];
-
-    // We are sorting rest elemets of array depending on the current sortKind. 
-    // Then run again from bottom to top, moving every found favorite to the top, 
-    // that makes multiply favorites be sored in the same way.
-    let sorted = [...result];
-
-    sorted.sort(compareSortRestaurants.get(this.state.sortKind))
-
-/*
-    for (let i = sorted.length - 1; i >= 0; i--) {
-      if (sorted[i].isFavorite)
-    }
-*/
-    return sorted;
+    return getFilterdRestaurantsList(
+      this.state.searchText,
+      this.state.sortKind
+    );
   }
 
   onListItemClick = item => {
@@ -55,12 +29,12 @@ export class ListOfRestaurants extends Component {
   };
 
   onSearchChange = event => {
-    this.setState({searchText: event.target.value})
+    this.setState({ searchText: event.target.value });
   };
 
   onSortChange = event => {
     const value = Number(event.target.value);
-    this.setState({sortKind: value})
+    this.setState({ sortKind: value });
   };
 
   render() {
--- src/storage/reataurants.js
+++ src/storage/reataurants.js
@@ -1,32 +1,32 @@
-import { getStorage, setStorage } from './storage'
+import { getStorage, setStorage } from "./storage";
 
 //------------------------------------------------------------------------------
 // Set of titles for sortKind values
 export const titleSortRestaurants = new Map();
-titleSortRestaurants.set(0, '[none]');
-titleSortRestaurants.set(1, 'best match');
-titleSortRestaurants.set(2, 'newest');
-titleSortRestaurants.set(3, 'rating average');
-titleSortRestaurants.set(4, 'distance');
-titleSortRestaurants.set(5, 'popularity');
-titleSortRestaurants.set(6, 'average product price');
-titleSortRestaurants.set(7, 'delivery costs');
-titleSortRestaurants.set(8, 'minimum costs');
-titleSortRestaurants.set(9, 'top restaurant');
+titleSortRestaurants.set(0, "[none]");
+titleSortRestaurants.set(1, "best match");
+titleSortRestaurants.set(2, "newest");
+titleSortRestaurants.set(3, "rating average");
+titleSortRestaurants.set(4, "distance");
+titleSortRestaurants.set(5, "popularity");
+titleSortRestaurants.set(6, "average product price");
+titleSortRestaurants.set(7, "delivery costs");
+titleSortRestaurants.set(8, "minimum costs");
+titleSortRestaurants.set(9, "top restaurant");
 
 //------------------------------------------------------------------------------
 // Set of property names for "restaurants.sortingValues" object
 export const propertySortRestaurants = new Map();
-propertySortRestaurants.set(0, '');
-propertySortRestaurants.set(1, 'bestMatch');
-propertySortRestaurants.set(2, 'newest');
-propertySortRestaurants.set(3, 'ratingAverage');
-propertySortRestaurants.set(4, 'distance');
-propertySortRestaurants.set(5, 'popularity');
-propertySortRestaurants.set(6, 'averageProductPrice');
-propertySortRestaurants.set(7, 'deliveryCosts');
-propertySortRestaurants.set(8, 'minCost');
-propertySortRestaurants.set(9, 'topRestaurants');
+propertySortRestaurants.set(0, "");
+propertySortRestaurants.set(1, "bestMatch");
+propertySortRestaurants.set(2, "newest");
+propertySortRestaurants.set(3, "ratingAverage");
+propertySortRestaurants.set(4, "distance");
+propertySortRestaurants.set(5, "popularity");
+propertySortRestaurants.set(6, "averageProductPrice");
+propertySortRestaurants.set(7, "deliveryCosts");
+propertySortRestaurants.set(8, "minCost");
+propertySortRestaurants.set(9, "topRestaurants");
 
 //------------------------------------------------------------------------------
 // Set of compare functions to sort "restaurants" array depending on sortKind value
@@ -48,7 +48,9 @@ compareSortRestaurants.set(5, (a, b) => {
   return b.sortingValues.popularity - a.sortingValues.popularity;
 });
 compareSortRestaurants.set(6, (a, b) => {
-  return b.sortingValues.averageProductPrice - a.sortingValues.averageProductPrice;
+  return (
+    b.sortingValues.averageProductPrice - a.sortingValues.averageProductPrice
+  );
 });
 compareSortRestaurants.set(7, (a, b) => {
   return b.sortingValues.deliveryCosts - a.sortingValues.deliveryCosts;
@@ -80,4 +82,58 @@ export function extendRestaurantsData() {
   }
 
   setStorage({ ...storage, isDataExtended: true });
+}
+
+//------------------------------------------------------------------------------
+// Returns array of restaurants sorted by "status" state: open -> order ahead -> closed.
+function sortRestaurantsDefault(list) {
+  let top = [];
+  let middle = [];
+  let bottom = [];
+
+  for (let i = 0; i < list.length; i++) {
+    let item = list[i];
+    // Compare text from short to long
+    if (item.status === "open") top.push(item);
+    else if (item.status === "closed") bottom.push(item);
+    else middle.push(item);
+  }
+
+  return [...top, ...middle, ...bottom];
+}
+
+//------------------------------------------------------------------------------
+// Sort function to move "favorite" item to the top
+function sortByFavorites(a, b) {
+  if (a.isFavorite && !b.isFavorite) return -1;
+  if (!a.isFavorite && b.isFavorite) return 1;
+  return 0;
+}
+
+//------------------------------------------------------------------------------
+// Returns array of restaurants depending on Search Text, Sorting Kind and Favorite state.
+// Favorites on the top and also sorted by sortKind value.
+export function getFilterdRestaurantsList(searchText = "", sortKind = 0) {
+  const storage = getStorage();
+  let list = storage.restaurants;
+  let result = [];
+
+  // Text Search filter (ignore case)
+  const search = searchText.trim().toLowerCase();
+  if (search !== "") {
+    for (let i = 0; i < list.length; i++) {
+      if (list[i].name.toLowerCase().includes(search)) result.push(list[i]);
+    }
+  } else result = [...list];
+
+  // We are sorting rest elemets of array depending on the current sortKind or by default sorting.
+  if (sortKind < 1 || sortKind >= compareSortRestaurants.length)
+    result = sortRestaurantsDefault(result);
+  else 
+    result.sort(compareSortRestaurants.get(sortKind));
+
+  // Run thru array again moving every found favorite to the top, that makes multiply favorites be sored in the same way.
+  result.sort(sortByFavorites);
+
+  return result;
 }
